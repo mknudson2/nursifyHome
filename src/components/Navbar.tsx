949c71b856fef3ad1c7e7898c63ba151f6152391
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2024-08-01 12:08:29 -0600

    Fixed navbar and offcanvas menu along with styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,25 +3,29 @@ import { BsList } from "react-icons/bs";
 
 function Navbar() {
     return (    
-    <nav className="navbar">
-        <div className="container">
+    <nav className="navbar-container">
+        <div className="navigation-container">
             <a className="navbar-brand" href="#">
-                <img src="public/NursifyLogo.webp" alt="Nursify Education" width="150" height="75"/>
+                <img src="/NursifyLogo.webp" alt="Nursify Education" width="175" height="100"/>
             </a>
-            <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"> 
-            <IconContext.Provider value={{size: "20px"}}>
-
-            <BsList/>
-            </IconContext.Provider>
+            <button className="btn btn-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"> 
+                <IconContext.Provider value={{size: "25px", color: "var(--accent-color)"}}>
+                    <BsList/>
+                </IconContext.Provider>
             </button>
 
-            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel" style={{ background: 'var(--body-bkg)' }}>
                 <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasRightLabel">Menu</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <h5 className="offcanvas-title" id="offcanvasRightLabel">
+                        <img src="/NursifyLogo.webp" alt="Nursify Education" width="150" height="75"/>
+                    </h5>
+                    <button type="button" className="btn-close text-bg-secondary" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div className="offcanvas-body">
-                 ...
+                 <p>About Us</p>
+                 <p>Services</p>
+                 <p>Contact Us</p>
+                 <button className="exp-btn">Join Nursify Now!</button>
                 </div>
             </div>
         </div>
